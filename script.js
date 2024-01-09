032e7e95d6a491b59bf7f2c60e2dadd0d97342c0
Ahzi <70834999+ahzi-pav@users.noreply.github.com>
2024-01-10 03:55:16 +0800

    Add screen and navigation pane

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const container = document.createElement('div');
-
+container.setAttribute('id', 'container');
 document.body.appendChild(container);
+
+const navPane = document.createElement('div');
+navPane.setAttribute('id', 'nav-pane');
+container.appendChild(navPane);
+
+const screen = document.createElement('div');
+screen.setAttribute('id', 'screen');
+container.appendChild(screen);
+
+function generateGrids(size) {
+    for (i = 0; i < size; i++) {
+        const column = document.createElement('div');
+        for (j = 0; j < size; j++) {
+            const row = document.createElement('div');
+            row.style.cssText = `height: ${500/size}px; width: ${500/size}px;`;
+            row.classList.add('cell');
+            column.appendChild(row);
+        }
+        screen.appendChild(column);
+    }
+}
+
+generateGrids(16);
