e103be9ed97034a5e0073098303b984a4eb94e20
Ahzi <70834999+ahzi-pav@users.noreply.github.com>
2024-01-10 08:05:37 +0800

    Add draw on screen functionality

--- script.js
+++ script.js
@@ -14,13 +14,39 @@ function generateGrids(size) {
     for (i = 0; i < size; i++) {
         const column = document.createElement('div');
         for (j = 0; j < size; j++) {
-            const row = document.createElement('div');
-            row.style.cssText = `height: ${500/size}px; width: ${500/size}px;`;
-            row.classList.add('cell');
-            column.appendChild(row);
-        }
+            const cell = document.createElement('div');
+            cell.style.cssText = `height: ${500/size}px; width: ${500/size}px;`;
+            cell.classList.add('cell');
+            cell.addEventListener('mousedown', () => {
+                cell.style.background = 'black';
+            });
+            column.appendChild(cell);
+        };
         screen.appendChild(column);
-    }
-}
+    };
+};
+
+const titleContainer = document.createElement('div');
+titleContainer.setAttribute('id', 'title-container');
+container.appendChild(titleContainer);
+
+const title = document.createElement('h1');
+title.setAttribute('id', 'title');
+title.textContent = "Etch-a-Sketch";
+titleContainer.appendChild(title);
 
 generateGrids(16);
+
+const cells = document.querySelectorAll('.cell');
+
+screen.addEventListener('mousedown', () => {
+    cells.forEach((cell) => {
+        cell.onmouseover = () => cell.style.background = 'black';
+    });
+})
+
+screen.addEventListener('mouseup', () => {
+    cells.forEach((cell) => {
+        cell.onmouseover = () => null;
+    });
+})
